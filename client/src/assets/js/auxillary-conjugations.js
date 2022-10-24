e61d5c810c60d22c8319eb511d3ef2fd00aef489
Benjamin Pike <benjpike1@gmail.com>
2022-10-25 00:05:35 +0100

    Added auxillary conjugations file

---
+++ client/src/assets/js/auxillary-conjugations.js
@@ -0,0 +1,428 @@
+export default {
+	spanish: {
+		indicative: {
+			present: {
+				yo: "he",
+				tú: "has",
+				él: "ha",
+				ella: "ha",
+				usted: "ha",
+				nosotros: "hemos",
+				vosotros: "habéis",
+				ellos: "han",
+				ellas: "han",
+				ustedes: "han"
+			},
+			preterite: {
+				yo: "hube",
+				tú: "hubiste",
+				él: "hubo",
+				ella: "hubo",
+				usted: "hubo",
+				nosotros: "hubimos",
+				vosotros: "hubisteis",
+				ellos: "hubieron",
+				ellas: "hubieron",
+				ustedes: "hubieron"
+			},
+			imperfect: {
+				yo: "había",
+				tú: "habías",
+				él: "había",
+				ella: "había",
+				usted: "había",
+				nosotros: "habíamos",
+				vosotros: "habíais",
+				ellos: "habían",
+				ellas: "habían",
+				ustedes: "habían"
+			},
+			conditional: {
+				yo: "habría",
+				tú: "habrías",
+				él: "habría",
+				ella: "habría",
+				usted: "habría",
+				nosotros: "habríamos",
+				vosotros: "habríais",
+				ellos: "habrían",
+				ellas: "habrían",
+				ustedes: "habrían"
+			},
+			future: {
+				yo: "habré",
+				tú: "habrás",
+				él: "habrá",
+				ella: "habrá",
+				usted: "habrá",
+				nosotros: "habremos",
+				vosotros: "habréis",
+				ellos: "habrán",
+				ellas: "habrán",
+				ustedes: "habrán"
+			}
+		},
+		subjunctive: {
+			present: {
+				yo: "haya",
+				tú: "hayas",
+				él: "haya",
+				ella: "haya",
+				usted: "haya",
+				nosotros: "hayamos",
+				vosotros: "hayáis",
+				ellos: "hayan",
+				ellas: "hayan",
+				ustedes: "hayan"
+			},
+			imperfect: {
+				yo: "hubiera",
+				tú: "hubieras",
+				él: "hubiera",
+				ella: "hubiera",
+				usted: "hubiera",
+				nosotros: "hubiéramos",
+				vosotros: "hubierais",
+				ellos: "hubieran",
+				ellas: "hubieran",
+				ustedes: "hubieran"
+			},
+			future: {
+				yo: "hubiere",
+				tú: "hubieres",
+				él: "hubiere",
+				ella: "hubiere",
+				usted: "hubiere",
+				nosotros: "hubiéremos",
+				vosotros: "hubiereis",
+				ellos: "hubieren",
+				ellas: "hubieren",
+				ustedes: "hubieren"
+			}
+		}
+	},
+	french: {
+		indicative: {
+			present: {
+				je: "ai",
+				tu: "as",
+				il: "a",
+				elle: "a",
+				ons: "a",
+				nous: "avons",
+				vous: "avez",
+				ils: "ont",
+				elles: "ont"
+			},
+			preterite: {
+				je: "eus",
+				tu: "eus",
+				il: "eut",
+				elle: "eut",
+				ons: "eut",
+				nous: "eûmes",
+				vous: "eûtes",
+				ils: "eurent",
+				elles: "eurent"
+			},
+			imperfect: {
+				je: "avais",
+				tu: "avais",
+				il: "avait",
+				elle: "avait",
+				ons: "avait",
+				nous: "avions",
+				vous: "aviez",
+				ils: "avaient",
+				elles: "avaient"
+			},
+			future: {
+				je: "aurai",
+				tu: "auras",
+				il: "aura",
+				elle: "aura",
+				ons: "aura",
+				nous: "aurons",
+				vous: "aurez",
+				ils: "auront",
+				elles: "auront"
+			}
+		},
+		subjunctive: {
+			present: {
+				je: "aie",
+				tu: "aies",
+				il: "ait",
+				elle: "ait",
+				ons: "ait",
+				nous: "ayons",
+				vous: "ayez",
+				ils: "aient",
+				elles: "aient"
+			},
+			imperfect: {
+				je: "eusse",
+				tu: "eusses",
+				il: "eût",
+				elle: "eût",
+				ons: "eût",
+				nous: "eussions",
+				vous: "eussiez",
+				ils: "eussent",
+				elles: "eussent"
+			}
+		},
+		conditional: {
+			conditional: {
+				je: "aurais",
+				tu: "aurais",
+				il: "aurait",
+				elle: "aurait",
+				ons: "aurait",
+				nous: "aurions",
+				vous: "auriez",
+				ils: "auraient",
+				elles: "auraient"
+			}
+		}
+	},
+    german: {
+        "indicative": {
+            "present": {
+                "ich": "habe",
+                "du": "hast",
+                "er": "hat",
+                "sie": "hat",
+                "es": "hat",
+                "wir": "haben",
+                "ihr": "habt",
+                "Sie": "haben"
+            },
+            "imperfect": {
+                "ich": "hatte",
+                "du": "hattest",
+                "er": "hatte",
+                "sie": "hatte",
+                "es": "hatte",
+                "wir": "hatten",
+                "ihr": "hattet",
+                "Sie": "hatten"
+            },
+            "future": {
+                "ich": "werde haben",
+                "du": "wirst haben",
+                "er": "wird haben",
+                "sie": "wird haben",
+                "es": "wird haben",
+                "wir": "werden haben",
+                "ihr": "werdet haben",
+                "Sie": "werden haben"
+            }
+        },
+        "subjunctive": {
+            "present": {
+                "ich": "habe",
+                "du": "habest",
+                "er": "habe",
+                "sie": "habe",
+                "es": "habe",
+                "wir": "haben",
+                "ihr": "habet",
+                "Sie": "haben"
+            },
+            "imperfect": {
+                "ich": "hätte",
+                "du": "hättest",
+                "er": "hätte",
+                "sie": "hätte",
+                "es": "hätte",
+                "wir": "hätten",
+                "ihr": "hättet",
+                "Sie": "hätten"
+            },
+            "future": {
+                "ich": "werde haben",
+                "du": "werdest haben",
+                "er": "werde haben",
+                "sie": "werde haben",
+                "es": "werde haben",
+                "wir": "werden haben",
+                "ihr": "werdet haben",
+                "Sie": "werden haben"
+            },
+            "conditional": {
+                "ich": "würde haben",
+                "du": "würdest haben",
+                "er": "würde haben",
+                "sie": "würde haben",
+                "es": "würde haben",
+                "wir": "würden haben",
+                "ihr": "würdet haben",
+                "Sie": "würden haben"
+            }
+        }
+    },
+	italian: {
+		indicative: {
+			present: {
+				io: "ho",
+				tu: "hai",
+				lui: "ha",
+				lei: "ha",
+				noi: "abbiamo",
+				voi: "avete",
+				loro: "hanno"
+			},
+			preterite: {
+				io: "ebbi",
+				tu: "avesti",
+				lui: "ebbe",
+				lei: "ebbe",
+				noi: "avemmo",
+				voi: "aveste",
+				loro: "ebbero"
+			},
+			imperfect: {
+				io: "avevo",
+				tu: "avevi",
+				lui: "aveva",
+				lei: "aveva",
+				noi: "avevamo",
+				voi: "avevate",
+				loro: "avevano"
+			},
+			future: {
+				io: "avrò",
+				tu: "avrai",
+				lui: "avrà",
+				lei: "avrà",
+				noi: "avremo",
+				voi: "avrete",
+				loro: "avranno"
+			}
+		},
+		subjunctive: {
+			present: {
+				io: "abbia",
+				tu: "abbia",
+				lui: "abbia",
+				lei: "abbia",
+				noi: "abbiamo",
+				voi: "abbiate",
+				loro: "abbiano"
+			},
+			imperfect: {
+				io: "avessi",
+				tu: "avessi",
+				lui: "avesse",
+				lei: "avesse",
+				noi: "avessimo",
+				voi: "aveste",
+				loro: "avessero"
+			}
+		},
+		conditional: {
+			conditional: {
+				io: "avrei",
+				tu: "avresti",
+				lui: "avrebbe",
+				lei: "avrebbe",
+				noi: "avremmo",
+				voi: "avreste",
+				loro: "avrebbero"
+			}
+		}
+	},
+	portuguese: {
+		indicative: {
+			present: {
+				eu: "tenho",
+				tu: "tens",
+				ele: "tem",
+				ela: "tem",
+				você: "tem",
+				nós: "temos",
+				vós: "tendes",
+				eles: "têm",
+				elas: "têm",
+				vocês: "têm"
+			},
+			imperfect: {
+				eu: "tinha",
+				tu: "tinhas",
+				ele: "tinha",
+				ela: "tinha",
+				você: "tinha",
+				nós: "tínhamos",
+				vós: "tínheis",
+				eles: "tinham",
+				elas: "tinham",
+				vocês: "tinham"
+			},
+			future: {
+				eu: "terei",
+				tu: "terás",
+				ele: "terá",
+				ela: "terá",
+				você: "terá",
+				nós: "teremos",
+				vós: "tereis",
+				eles: "terão",
+				elas: "terão",
+				vocês: "terão"
+			}
+		},
+		subjunctive: {
+			present: {
+				eu: "tenha",
+				tu: "tenhas",
+				ele: "tenha",
+				ela: "tenha",
+				você: "tenha",
+				nós: "tenhamos",
+				vós: "tenhais",
+				eles: "tenham",
+				elas: "tenham",
+				vocês: "tenham"
+			},
+			imperfect: {
+				eu: "tivesse",
+				tu: "tivesses",
+				ele: "tivesse",
+				ela: "tivesse",
+				você: "tivesse",
+				nós: "tivéssemos",
+				vós: "tivésseis",
+				eles: "tivessem",
+				elas: "tivessem",
+				vocês: "tivessem"
+			},
+			future: {
+				eu: "tiver",
+				tu: "tiveres",
+				ele: "tiver",
+				ela: "tiver",
+				você: "tiver",
+				nós: "tivermos",
+				vós: "tiverdes",
+				eles: "tiverem",
+				elas: "tiverem",
+				vocês: "tiverem"
+			}
+		},
+		conditional: {
+			conditional: {
+				eu: "teria",
+				tu: "terias",
+				ele: "teria",
+				ela: "teria",
+				você: "teria",
+				nós: "teríamos",
+				vós: "teríeis",
+				eles: "teriam",
+				elas: "teriam",
+				vocês: "teriam"
+			}
+		}
+	}
+};
